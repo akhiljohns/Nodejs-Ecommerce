353115d93b6f200f6c2a4291fd7a1cce0b3b22ff
Akhil Johns <100527835+akhiljohns@users.noreply.github.com>
2024-04-17 21:27:42 +0530

    removed logging

--- helpers/product-helper.js
+++ helpers/product-helper.js
@@ -9,7 +9,6 @@ module.exports = {
         return new Promise((resolve, reject) => {
             db.get().collection(collection.PRODUCT_COLLECTION).insertOne(product).then(async(result) => {
                 let product = await db.get().collection(collection.PRODUCT_COLLECTION).findOne({ _id: ObjectId(result.insertedId) })
-                console.log(product)
                 resolve(product);
             })
         })
